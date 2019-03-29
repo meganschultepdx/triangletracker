cf13ef392aeffbd5ec965b19672ae12fa27f9eae
megan schulte <megan.schulte.pdx@gmail.com>
2019-03-28 22:42:42 -0700

    try to fix bug with function, did not work

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,45 @@
 $(document).ready(function() {
   $("#quizform").submit(function(event) {
 
-    var side1 = parseInt($("#side1")).val();
-    var side2 = parseInt($("#side2")).val();
-    var side3 = parseInt($("#side3")).val();
+  var side1Input = parseInt($("input#side1")).val();
+  var side1 = parseInt(prompt("how old?"));
+  var side2 = parseInt($("#side2")).val();
+  // var side3 = parseInt($("#side3")).val();
 
-  if (side1 === side2 && side2 === side3) {
-    $("#equilateral").show();
-    $("#scalene, #isosceles").hide();
-
-  } else if (side1 === side2 && side2 != side3 || side1 === side3 && side3 != side2 || side2 === side3 && side3 != side1) {
-    $("#isosceles").show();
-    $("#scalene, #equilateral").hide();
-  // } else if (side1 === side3 && side3 != side2) {
-  //   $("#isosceles").show();
-  //   $("#scalene, #equilateral").hide();
-  // } else if (side2 === side3 && side3 != side1) {
-  //   $("#isosceles").show();
-  //   $("#scalene, #equilateral").hide();
-  } else if (side1 != side2 && side2 != side3 || side1 != side3 && side3 != side2) {
-    $("#scalene").show();
-    $("#isosceles, #equilateral").hide();
+  if (side1 === side2) {
+    // $("#equilateral").show();
+    alert("help");
   }
-  // } else if (side1 != side3 && side3 != side2) {
-  //   $("#scalene").show();
-  //   $("#isosceles, #equilateral").hide();
-  // }
 
-  event.preventDefault();
+
+    // if (side1 === side2 && side2 === side3) {
+    //   $("#equilateral").show();
+    //   $("#scalene, #isosceles").hide();
+    //
+    // } else if (side1 === side2 && side2 != side3 || side1 === side3 && side3 != side2 || side2 === side3 && side3 != side1) {
+    //   $("#isosceles").show();
+    //   $("#scalene, #equilateral").hide();
+    //
+    // } else if (side1 != side2 && side2 != side3 || side1 != side3 && side3 != side2) {
+    //   $("#scalene").show();
+    //   $("#isosceles, #equilateral").hide();
+    // }
+
+
+
+    // } else if (side1 === side3 && side3 != side2) {
+    //   $("#isosceles").show();
+    //   $("#scalene, #equilateral").hide();
+    // } else if (side2 === side3 && side3 != side1) {
+    //   $("#isosceles").show();
+    //   $("#scalene, #equilateral").hide();
+
+    // } else if (side1 != side3 && side3 != side2) {
+    //   $("#scalene").show();
+    //   $("#isosceles, #equilateral").hide();
+    // }
+
+    event.preventDefault();
 
   });
 });
