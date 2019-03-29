3405723cedd557e1ad8e504804238b6e86e1d4a9
megan schulte <megan.schulte.pdx@gmail.com>
2019-03-28 21:57:53 -0700

    change js to make branching work, still not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,31 @@
-s(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    var side1 = parseInt($("side1")).val();
-    var side2 = parseInt($("side2")).val();
-    var side3 = ($("side3")).val();
+$(document).ready(function() {
+  $("#quizform").submit(function(event) {
 
-  if (side1 === side2 && side2  === side3 && side3 ==== side1) {
-    $("equilateral").show();
-    $("#not, #scalene, #isosceles ").hide();
-  } else if (side1 === side2 && side2 ! side3) {
-    $("isosceles").show();
-    $("#not, #scalene, #equilateral").hide();
-  } else if (side1 === side3 && side3 ! side2) {
-    $("isosceles").show();
-    $("#not, #scalene, #equilateral").hide();
-  } else if (side2 === side3 && side3 ! side1) {
-    $("isosceles").show();
-    $("#not, #scalene, #equilateral").hide();
-  } else if (side1 ! side2 && side2 ! side3) {
-    $("scalene").show();
-    $("#not, #isosceles, #equilateral").hide();
-  } else if (side1 ! side3 && side3 ! side2) {
-    $("scalene").show();
-    $("#not, #isosceles, #equilateral").hide();
+    var side1 = parseInt($("#side1")).val();
+    var side2 = parseInt($("#side2")).val();
+    var side3 = parseInt($("#side3")).val();
+
+  if (side1 === side2 && side2 === side3) {
+    $("#equilateral").show();
+    $("#scalene, #isosceles").hide();
+
+  } else if (side1 === side2 && side2 != side3 || side1 === side3 && side3 != side2 || side2 === side3 && side3 != side1) {
+    $("#isosceles").show();
+    $("#scalene, #equilateral").hide();
+  // } else if (side1 === side3 && side3 != side2) {
+  //   $("#isosceles").show();
+  //   $("#scalene, #equilateral").hide();
+  // } else if (side2 === side3 && side3 != side1) {
+  //   $("#isosceles").show();
+  //   $("#scalene, #equilateral").hide();
+  } else if (side1 != side2 && side2 != side3 || side1 != side3 && side3 != side2) {
+    $("#scalene").show();
+    $("#isosceles, #equilateral").hide();
   }
+  // } else if (side1 != side3 && side3 != side2) {
+  //   $("#scalene").show();
+  //   $("#isosceles, #equilateral").hide();
+  // }
 
   event.preventDefault();
 
