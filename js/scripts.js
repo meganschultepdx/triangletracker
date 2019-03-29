645ee3765ee17a97b37b2f5cfe3a8e290c344a0b
megan schulte <megan.schulte.pdx@gmail.com>
2019-03-28 21:13:21 -0700

    add branches to scritps.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,27 @@ s(document).ready(function() {
     var side2 = parseInt($("side2")).val();
     var side3 = ($("side3")).val();
 
-  if
-    $("#results").show();
-    $("#results").hide();
-
-    event.preventDefault();
+  if (side1 === side2 && side2  === side3 && side3 ==== side1) {
+    $("equilateral").show();
+    $("#not, #scalene, #isosceles ").hide();
+  } else if (side1 === side2 && side2 ! side3) {
+    $("isosceles").show();
+    $("#not, #scalene, #equilateral").hide();
+  } else if (side1 === side3 && side3 ! side2) {
+    $("isosceles").show();
+    $("#not, #scalene, #equilateral").hide();
+  } else if (side2 === side3 && side3 ! side1) {
+    $("isosceles").show();
+    $("#not, #scalene, #equilateral").hide();
+  } else if (side1 ! side2 && side2 ! side3) {
+    $("scalene").show();
+    $("#not, #isosceles, #equilateral").hide();
+  } else if (side1 ! side3 && side3 ! side2) {
+    $("scalene").show();
+    $("#not, #isosceles, #equilateral").hide();
   }
-}
+
+  event.preventDefault();
+
+  });
+});
